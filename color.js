a913c61915a440acb26b4e2cc421fbdb3e3bedc0
emai-lai <46794504+emai-lai@users.noreply.github.com>
2019-02-09 20:26:39 -0500

    Add files via upload

---
+++ color.js
@@ -0,0 +1,36 @@
+<script type = "text/javascript">
+
+<!--
+
+// Comment
+
+/*
+
+EVERYTHING IN GRAY IS COMMENT
+
+
+DEFINTELY
+bright poppydark bold red, = "The Middle" (Coca Cola)
+pastels, pink, blue, purple = "Hell Nos and Headphones" (HackHer413)
+yellow, and dark blue, pink? = can't stop this feeling!!!!  
+black, neon green = high hopes (Monster energy)
+
+EH MAYBE
+yellow = journey to the past?
+dark angry red = i didn't plan it
+
+green/blue paris in the rain
+purple = Me & you
+
+
+
+
+
+
+*/
+
+var title = "JavaScript console in Sublime Text";
+console.log(title);
+
+
+</script>
